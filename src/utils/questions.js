4ba68dc91754755f4370be479380aec014d3ac66
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-05-26 18:39:27 +0100

    questions are added

---
+++ src/utils/questions.js
@@ -0,0 +1,26 @@
+//questions
+const questions = [
+  {
+    type: "list",
+    messages: "What would you like to do?",
+    choices: [
+      "View All Employees",
+      "View All Departments",
+      "View All Roles",
+      "View All Employees By Department",
+      "View All Employees By Manager",
+      "Add Employee",
+      "Add Department",
+      "Add Role",
+      "Remove Employee",
+      "Remove Department",
+      "Remove Role",
+      "Update Employee",
+      "Update Employee Role",
+      "Update Employee Manager",
+      "View Total Budget By Department",
+      "Exit",
+    ],
+    name: "chooseAction",
+  },
+];
