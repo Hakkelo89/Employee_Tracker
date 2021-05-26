97652bf6fda136cb05d2c545aafe4fa801b450c1
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-05-26 19:15:11 +0100

    application start check

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
+
+const Questions = require("./lib/questions");
+const UserSearch = require("./lib/UserSearch");
+
+const questions = new Questions();
+const newSearch = new UserSearch();
+
+const init = () => {
+  console.log("Welcome to Employee Tracker");
+  loadMenu();
+};
