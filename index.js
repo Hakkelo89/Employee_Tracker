386672a265fcbbeb803cf1e279744075728a3f4e
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-05-26 21:26:17 +0100

    schema.sql updated

--- index.js
+++ index.js
@@ -1,261 +1,100 @@
-//npm requires
 const inquirer = require("inquirer");
-
-const consoleTable = require("console.table");
-
-const Questions = require("./lib/questions");
-const UserSearch = require("./lib/userSearch");
-
-const questions = new Questions();
-const newSearch = new UserSearch();
-
-const init = () => {
-  console.log("Welcome to Employee Tracker");
-  loadMenu();
-};
-
-const loadMenu = async () => {
-  await inquirer.prompt(questions.startPage).then(async (answers) => {
-    switch (answers.choice) {
-      case "View All Employees":
-        await getAllEmployeesFullData();
-        break;
-      case "View All Departments":
-        await viewAllDepartments();
-        break;
-      case "View All Roles":
-        await viewAllRoles();
-        break;
-      case "View All Employees By Department":
-        await viewAllEmployeesByManager();
-        break;
-      case "View All Employees By Manager":
-        await viewAllEmployeesByManager();
-        break;
-      case "Add Employee":
-        await addEmployee();
-        break;
-      case "Add Department":
-        await addDepartment();
-        break;
-      case "Add Role":
-        await addRole();
-        break;
-      case "Update Employee Role":
-        await updateEmployeeRole();
-        break;
-      case "Update Employee Manager":
-        await updateEmployeeManager();
-        break;
-      case "View Total Budget By Department":
-        await updateTotalBudgetByDepartment();
-        break;
-
-      case "Exit":
-        break;
-      default:
-        break;
-    }
-  });
-};
-
-const viewEmployeesByManager = async () => {
-  let managerList;
-  let managers;
-
-  await newSearch.getAllManagerNames().then((res) => {
-    managers = res;
-    managerList = managers.map((employee) => employee.name);
-    managerList.push("Cancel");
-  });
-
-  await inquirer
-    .prompt({
-      message: "Choose a manager:",
+const DB = require("./db/DB");
+const init = async () => {
+  const db = new DB("company_db");
+  await db.start();
+  let inProgress = true;
+  while (inProgress) {
+    const question = {
+      name: "action",
       type: "list",
-      choices: managerList,
-      name: "choice",
-    })
-    .then(async function (answer) {
-      switch (answer.choice) {
-        case "Cancel":
-          break;
-        default:
-          manager = managers.find(
-            (employee) => employee.name === answer.choice
-          );
-          await newSearch.getEmployeesByManager(manager).then((res) => {
-            console.table(res);
-          });
-          break;
+      message: "What would you like to do?",
+      choices: [
+        {
+          short: "Employees",
+          value: "viewAllEmployees",
+          name: "View All Employees",
+        },
+        {
+          short: "Employees By Department",
+          value: "viewAllEmployeesByDepartment",
+          name: "View All Employees By Department",
+        },
+        {
+          short: "Employees By Role",
+          value: "viewAllEmployeesByRole",
+          name: "View All Employees By Role",
+        },
+        {
+          short: "Add Employee",
+          value: "addEmployee",
+          name: "Add an Employee",
+        },
+        {
+          short: "Remove Employee",
+          value: "removeEmployee",
+          name: "Remove an Employee",
+        },
+        {
+          value: "updateEmployee",
+          name: "Update an Employee",
+        },
+        {
+          value: "updateEmployeeRole",
+          name: "Update Employee Role",
+        },
+        {
+          value: "updateEmployeeManager",
+          name: "Update Employee Manager",
+        },
+        {
+          short: "Roles",
+          value: "viewAllRoles",
+          name: "View All Roles",
+        },
+        {
+          value: "addRole",
+          name: "Add Role",
+        },
+        {
+          value: "removeRole",
+          name: "Remove Role",
+        },
+        {
+          short: "Departments",
+          value: "viewAllDepartments",
+          name: "View All Departments",
+        },
+        {
+          value: "addDepartment",
+          name: "Add Departments",
+        },
+        {
+          value: "removeDepartment",
+          name: "Remove Departments",
+        },
+        {
+          short: "Budget",
+          value: "viewBudget",
+          name: "View Utilised Budget for a Department",
+        },
+        {
+          short: "Exit",
+          value: "exit",
+          name: "Exit",
+        },
+      ],
+    };
+    const answers = await inquirer.prompt(question);
+    if (answers.action === "exit") {
+      inProgress = false;
+    } else {
+      if (answers.action === "viewAllDepartments") {
+        const query = "SELECT * FROM department";
+        const data = await db.query(query);
+        console.table(data);
       }
-    });
-  loadMenu();
-};
-
-const getAllEmployeesFullData = async () => {
-  await newSearch.getAllEmployeesFullData().then((res) => {
-    console.table(res);
-  });
-  loadMenu();
-};
-
-const viewAllRoles = async () => {
-  await newSearch.viewAllRoles().then((res) => {
-    console.table(res);
-  });
-  loadMenu();
-};
-
-const viewAllDepartments = async () => {
-  await newSearch.getAllDepartments().then((res) => {
-    console.table(res);
-  });
-  loadMenu();
-};
-
-const addDepartment = async () => {
-  await inquirer.prompt(questions.addDepartment).then(async (answer) => {
-    await newSearch.addDepartment(answer.department).then((res) => {
-      console.log(`New Department ID: ${res}`);
-    });
-  });
-
-  loadMenu();
-};
-
-const addRole = async () => {
-  let question = questions.addRole;
-  let departments;
-  let departmentNames;
-
-  await newSearch.getAllDepartments().then((res) => {
-    departmentNames = res.map((employee) => employee.name);
-    departments = res;
-  });
-
-  //set the list of choices
-  question.find((employee) => employee.name === "department").choices =
-    departmentNames;
-  question.find((employee) => employee.name === "department").pageSize =
-    departmentNames.length;
-
-  await inquirer.prompt(question).then(async (answers) => {
-    let role = {
-      title: answers.title,
-      salary: answers.salary,
-      department_id: departments.find(
-        (employee) => employee.name === answers.department
-      ).id,
-    };
-
-    await newSearch.addRole(role).then((res) => {
-      console.log(res);
-    });
-  });
-
-  loadMenu();
-};
-
-const updateEmployeeRole = async () => {
-  let question = questions.updateEmployeeRole;
-  let confirm = questions.confirmInput;
-  let roles;
-  let roleNames;
-  let employees;
-  let employeeNames;
-  let employee;
-  let role;
-
-  await newSearch.getAllRoles().then((res) => {
-    roles = res;
-    roleNames = res.map((employee) => employee.title);
-  });
-
-  await newSearch.getAllEmployees().then((res) => {
-    employees = res;
-    employeeNames = res.map(
-      (employee) => `${employee.first_name} ${employee.last_name}`
-    );
-  });
-
-  question.find((employee) => employee.name === "employee").choices =
-    employeeNames;
-  question.find((employee) => employee.name === "employee").pageSize =
-    employeeNames.length;
-  question.find((employee) => employee.name === "role").choices = roleNames;
-  question.find((employee) => employee.name === "role").pageSize =
-    roleNames.length;
-
-  await inquirer.prompt(question).then(async (answers) => {
-    employee = employees.find(
-      (employee) =>
-        `${employee.first_name} ${employee.last_name}` === answers.employee
-    );
-    role = roles.find((employee) => employee.title === answers.role);
-  });
-
-  confirm.message = `Would you like to update the role of ${employee.first_name} ${employee.last_name} to ${role.title}? Please Confirm`;
-
-  await inquirer.prompt(confirm).then(async (answer) => {
-    if (answer.confirm) {
-      await newSearch.updateEmployeeRole(employee, role).then((res) => {
-        console.log(res);
-        return;
-      });
     }
-  });
-
-  loadMenu();
+  }
 };
-
-const addEmployee = async () => {
-  let question = questions.addEmployee;
-  let managers;
-  let managerNames;
-  let roles;
-  let roleNames;
-
-  await newSearch.getAllManagerNames().then((res) => {
-    managerNames = res.map((employee) => employee.name);
-    managers = res;
-    managerNames.push("None");
-  });
-
-  await newSearch.getAllRoles().then((res) => {
-    roles = res;
-    roleNames = res.map((employee) => employee.title);
-  });
-
-  question.find((employee) => employee.name === "role").choices = roleNames;
-  question.find((employee) => employee.name === "role").pageSize =
-    roleNames.length;
-  question.find((employee) => employee.name === "manager").choices =
-    managerNames;
-  question.find((employee) => employee.name === "manager").pageSize =
-    managerNames.length;
-
-  await inquirer.prompt(question).then(async (answers) => {
-    let role_id = roles.find((employee) => employee.title === answers.role).id;
-
-    let manager_id =
-      answers.manager === "None"
-        ? null
-        : managers.find((employee) => employee.name === answers.manager).id;
-
-    let employee = {
-      first_name: answers.first_name,
-      last_name: answers.last_name,
-      role_id: role_id,
-      manager_id: manager_id,
-    };
-
-    await newSearch.addEmployee(employee).then((res) => {
-      console.log("New Employee ID: " + res);
-    });
-  });
-
-  loadMenu();
-};
-
 init();
